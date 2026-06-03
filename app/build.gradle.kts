cb37f9ee3f29dc1e16a59c42da1984cb3c5b6f8f
233255277 <3039053739@qq.com>
2026-06-03 08:11:33 +0800

    v1.3.0: 隐藏多任务切换 + 权限禁用声明

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hdrscreen"
         minSdk = 34
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.3.0"
     }
 
     buildTypes {
